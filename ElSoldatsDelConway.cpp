35de46c948538ce84e2c4d4966b2138380084192
Salvador  DB <salvadordb310@gmail.com>
2026-03-02 01:03:58 +0100

    Col·lapse del pou infinit

--- ElSoldatsDelConway.cpp
+++ ElSoldatsDelConway.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-// falta que el tamaño de la matriz sea variable
-//  falta bajar el o(n)
-
 // --- Macros catalanes ---
 #define principal main
 #define buit void
@@ -23,16 +20,45 @@ buit mostraMatriu(const vector<vector<ent>>& m);
 buit FarlandsWell(ent liniaVermella);
 
 ent principal() {
-
     FarlandsWell(4);
-
     retorna 0;
 }
 
+/*
+L'algoritme opera sobre una matriu de mida m x 5, on m ∈ N i m > 0.
+Es defineix una 'línia vermella' (L) com un llindar on L ∈ N i 0 < L < m.
+L'estat inicial és un '1' a la posició central (fila 0, columna 2).
 
+Lògica de propagació:
 
+A partir de la fila L es prioritza el desplaçament lateral per tal d'alliberar la columna central.
+En qualsevol altre cas, la propagació continua cap avall.
+
+Punts de col·lapse:
+
+Si només deixem que els "1" s'apartin cap a un costat, ens trobem amb el següent bucle infinit:
+X X 1 0 0 
+1 1 X 0 0  
+
+0 0 1 0 0  |  0 0 0 0 0  |  0 0 0 0 0
+1 1 0 0 0  |  1 1 1 0 0  |  1 1 1 0 0
+0 0 0 0 0  |  0 0 1 0 0  |  1 1 0 0 0
+0 0 0 0 0  |  0 0 0 0 0  |  0 0 0 0 0
+
+Si deixem que els "1" s'apartin cap a banda i banda, ens trobem amb el següent bucle infinit:
+1 1 1 1 1 
+1 1 X 1 1 
+1 1 X 1 1
+1 1 X X X
+
+1 1 1 1 1  |  1 1 0 1 1  |  1 1 0 1 1
+1 1 0 1 1  |  1 1 1 1 1  |  1 1 1 1 1
+1 1 0 1 1  |  1 1 1 1 1  |  1 1 0 1 1
+1 1 0 0 0  |  1 1 0 0 0  |  1 1 0 1 1
+0 0 0 0 0  |  0 0 0 0 0  |  1 1 0 0 0
+*/
 buit FarlandsWell(ent liniaVermella) {
-    vector<vector<ent>> m(120, vector<ent>(5, 0));
+    vector<vector<ent>> m(20, vector<ent>(5, 0));
     m[0][2] = 1;
     bool aDaltBrut = cert;
 
@@ -40,7 +66,8 @@ buit FarlandsWell(ent liniaVermella) {
         aDaltBrut = fals;
         per (ent i = 0; i<m.mida(); i++) {
             si (m[i][2]==1) {
-                aDaltBrut = cert;
+                //aDaltBrut = cert;
+                si (i<liniaVermella) {aDaltBrut = cert;}
                 // Si estem per sota la línia vermella, intentem moure el "1" cap als costats.
                 si (i>=liniaVermella) {
                     si      (m[i][1]==0) {m[i][2] = 0; m[i][1] = 1; m[i][0] = 1;} 
@@ -50,11 +77,10 @@ buit FarlandsWell(ent liniaVermella) {
                 si (m[i][2]==1) {
                     si (i+2<m.mida() && m[i+1][2]+m[i+2][2]==0) {m[i][2] = 0; m[i+1][2] = 1;m[i+2][2] = 1;}
                 }
-            }
-            mostraMatriu(m); 
+            } 
         }
+        mostraMatriu(m);
     }   
-    mostraMatriu(m); 
 }
 
 buit mostraMatriu(const vector<vector<ent>>& m) {
